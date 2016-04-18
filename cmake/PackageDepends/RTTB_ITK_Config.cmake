1809df160ff31c42321d7ffc7f4925baf1847aa3
Ralf Floca <r.floca@dkfz-heidelberg.de>
2016-04-18 15:22:39 +0000

    fixed issue #2051

--- cmake/PackageDepends/RTTB_ITK_Config.cmake
+++ cmake/PackageDepends/RTTB_ITK_Config.cmake
@@ -15,4 +15,15 @@ LIST(APPEND ALL_LIBRARIES ${ITK_LIBRARIES})
 
 LINK_DIRECTORIES(${ITK_LIBRARY_DIRS})
 
+OPTION(RTTB_USE_SYSTEM_GDCM "Activate checker to choose a GDCM installation that should be linked with ITK. (This is needed e.g. when building against an ITK that is distributed with MITK. MITK uses its own GDCM and not the one distributed with ITK.)" OFF)     
+
+IF (RTTB_USE_SYSTEM_GDCM)
+  MESSAGE (STATUS "RTTB uses system GDCM instead of ITK distribution.")
+  FIND_PACKAGE(GDCM PATHS ${ITK_GDCM_DIR} ${GDCM_DIR} REQUIRED)
+  INCLUDE(${GDCM_USE_FILE})
+  LIST(APPEND ALL_INCLUDE_DIRECTORIES ${GDCM_INCLUDE_DIRS})
+  LIST(APPEND ALL_LIBRARIES ${GDCM_LIBRARIES})	    
+ENDIF (RTTB_USE_SYSTEM_GDCM)
+
+
 CONFIGURE_FILE(${RTToolbox_SOURCE_DIR}/cmake/ITKConfig.cmake.in ${RTTB_MODULES_CONF_DIR}/ITKConfig.cmake @ONLY)
