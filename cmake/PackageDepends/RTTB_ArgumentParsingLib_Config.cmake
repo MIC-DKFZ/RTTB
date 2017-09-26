9702a8d089371269f4f58213390997f749d6170c
Clemens Hentschke <c.hentschke@dkfz-heidelberg.de>
2017-09-26 17:30:45 +0200

    Resolved T23422. Adapted CMakeLists for GCC5. Added warning if GCC version is !=5

--- cmake/PackageDepends/RTTB_ArgumentParsingLib_Config.cmake
+++ cmake/PackageDepends/RTTB_ArgumentParsingLib_Config.cmake
@@ -5,7 +5,7 @@
 set(ArgumentParsingLib_INCLUDE_DIR ${ArgumentParsingLib_SOURCE_DIR}/main)
 set(ArgumentParsingLib_LIBRARY_DIR ${ArgumentParsingLib_BUILD_DIR}/main)
 set(ArgumentParsingLib_Boost_INCLUDE_DIR ${Boost_INCLUDE_DIR})
-set(ArgumentParsingLib_Boost_LIBRARY_DIR ${Boost_LIBRARY_DIR})
+set(ArgumentParsingLib_Boost_LIBRARY_DIR ${Boost_LIBRARYDIR})
 set(ArgumentParsingLib_Boost_LIBRARIES ${Boost_LIBRARIES})
 
 LIST(APPEND ALL_INCLUDE_DIRECTORIES ${ArgumentParsingLib_INCLUDE_DIR} ${ArgumentParsingLib_Boost_INCLUDE_DIR})
