aef3423bdd3911487b1be07fc7474d14ceb145ac
Timo Wiese <t.wiese@dkfz-heidelberg.de>
2018-04-06 14:27:33 +0200

    Move file and dir check

--- code/core/rttbUtils.h
+++ code/core/rttbUtils.h
@@ -12,12 +12,6 @@
 // PURPOSE.  See the above copyright notices for more information.
 //
 //------------------------------------------------------------------------
-/*!
-// @file
-// @version $Revision: 1674 $ (last changed revision)
-// @date    $Date: 2017-01-27 10:34:46 +0100 (Fr, 27 Jan 2017) $ (last change date)
-// @author  $Author: hentsch $ (last changed by)
-*/
 
 #ifndef __RTTB_UTILS_H
 #define __RTTB_UTILS_H
@@ -33,11 +27,15 @@ namespace rttb
 	namespace core
 	{
 
-    bool RTTBCore_EXPORT isKey(const std::map<double, double>& values, double value);
+		bool RTTBCore_EXPORT isKey(const std::map<double, double>& values, double value);
 
 		bool RTTBCore_EXPORT isKey(const std::vector<double>& values, double value);
 
 		bool RTTBCore_EXPORT valueIsClose(double value1, double value2, double specificErrorConstant = 1e-5);
+
+		bool RTTBCore_EXPORT isFile(FileNameType aName);
+
+		bool RTTBCore_EXPORT isDirectory(FileNameType aName);
 
 		std::string RTTBCore_EXPORT getFilenameWithoutEnding(const std::string& outfilename);
 	
