5a7452af3e19ec4f4e697a80ed9a6225a8623513
Timo Wiese <t.wiese@dkfz-heidelberg.de>
2018-07-24 11:11:29 +0200

    adapt interpolation

--- code/interpolation/rttbRosuMappableDoseAccessor.h
+++ code/interpolation/rttbRosuMappableDoseAccessor.h
@@ -12,12 +12,7 @@
 // PURPOSE.  See the above copyright notices for more information.
 //
 //------------------------------------------------------------------------
-/*!
-// @file
-// @version $Revision$ (last changed revision)
-// @date    $Date$ (last change date)
-// @author  $Author$ (last changed by)
-*/
+
 #ifndef __ROSU_MAPPABLE_DOSE_ACCESSOR_H
 #define __ROSU_MAPPABLE_DOSE_ACCESSOR_H
 
