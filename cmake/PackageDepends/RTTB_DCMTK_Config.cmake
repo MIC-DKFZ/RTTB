4bb4472b01aa7b94a49bb73a4e8882e0999ec231
Ralf Floca <r.floca@dkfz-heidelberg.de>
2022-09-08 10:42:54 +0200

    Changed the integration of DCMTK to CMake's component based build system

--- cmake/PackageDepends/RTTB_DCMTK_Config.cmake
+++ cmake/PackageDepends/RTTB_DCMTK_Config.cmake
@@ -2,9 +2,17 @@ if(NOT WIN32 AND NOT APPLE)
   set(MISSING_LIBS_REQUIRED_BY_DCMTK tiff z)
 endif()
 
-if(NOT DCMTK_FOUND)
-  find_package(DCMTK NO_MODULE REQUIRED)
-endif()
+foreach(dcmtk_component ${DCMTK_REQUIRED_COMPONENTS_BY_MODULE})
+  set(dcmtk_component "DCMTK::${dcmtk_component}")
+  list(APPEND _dcmtk_required_components_by_module ${dcmtk_component})
+endforeach()
+
+find_package(DCMTK COMPONENTS ${_dcmtk_required_components_by_module} REQUIRED)
+
+foreach(dcmtk_component ${_dcmtk_required_components_by_module})
+  list(APPEND ALL_LIBRARIES ${dcmtk_component})
+endforeach()
+
 
 list(APPEND ALL_INCLUDE_DIRECTORIES ${DCMTK_INCLUDE_DIRS})
-list(APPEND ALL_LIBRARIES ${DCMTK_LIBRARIES} ${MISSING_LIBS_REQUIRED_BY_DCMTK})
+list(APPEND ALL_LIBRARIES ${MISSING_LIBS_REQUIRED_BY_DCMTK})
